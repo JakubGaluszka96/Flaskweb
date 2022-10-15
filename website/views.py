7d3c8ce5ea31c2ef5546aae5eb49ba96c9380636
JakubGaluszka96 <81179005+JakubGaluszka96@users.noreply.github.com>
2022-10-16 00:19:36 +0200

    Add files via upload

---
+++ website/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    return render_template("home.html")
+
